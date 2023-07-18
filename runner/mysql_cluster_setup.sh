f66fff86b37ad3961be9ab1d4a698a644c5a5470
Egor <eozhmegov@ozon.ru>
2023-07-18 15:26:06 +0300

    runner added

--- runner/mysql_cluster_setup.sh
+++ runner/mysql_cluster_setup.sh
@@ -7,7 +7,7 @@ do
 done
 
 priv_stmt='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
-export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e '$priv_stmt'
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "$priv_stmt"
 
 until export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e ";"
 do
@@ -21,5 +21,5 @@ CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
 start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
 
-export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e '$start_slave_stmt'
-export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e 'SHOW SLAVE STATUS \G'
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "$start_slave_stmt"
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "SHOW SLAVE STATUS \G"
