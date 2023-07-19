a9aea896cb6be0e7f11872bf33217efbda1d9701
Egor <eozhmegov@ozon.ru>
2023-07-19 22:48:15 +0300

    runner added

--- runner/mysql_backup.sh
+++ runner/mysql_backup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-mysql -u root -h mysql_slave -e "STOP SLAVE;"
-MYSQL=`mysql -u root -h mysql_slave --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "STOP SLAVE;"
+MYSQL=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
     do
-    TABLES=`mysql -u root -h mysql_slave --skip-column-names -e "use test_db; show tables;"`
+    TABLES=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave --skip-column-names -e "use test_db; show tables;"`
     mkdir -m 777 /opt/mysql/backup/$database;
     for table in $TABLES;
         do
-        mysqldump -u root -h mysql_slave --add-drop-table --add-locks --create-options --disable-keys\
+        export MYSQL_PWD=rootpass; mysqldump -u root -h mysql_slave --add-drop-table --add-locks --create-options --disable-keys\
         --extended-insert --single-transaction --quick --set-charset --events --routines\
         --triggers --tab=/tmp/$database $database $table;
         done
     done
-mysql -u root -h mysql_slave -e "START SLAVE;"
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "START SLAVE;"
