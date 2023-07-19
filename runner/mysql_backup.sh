f358f0eae488fb0c5b176215043fce07c5908c6d
Egor <eozhmegov@ozon.ru>
2023-07-19 23:09:34 +0300

    runner added

--- runner/mysql_backup.sh
+++ runner/mysql_backup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+rm -r /opt/mysql/backup/*
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "STOP SLAVE;"
 MYSQL=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
     do
-    TABLES=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave --skip-column-names -e "use test_db; show tables;"`
+    TABLES=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave --skip-column-names -e "use $database; show tables;"`
     mkdir -m 777 /opt/mysql/backup/$database;
     for table in $TABLES;
         do
         export MYSQL_PWD=rootpass; mysqldump -u root -h mysql_slave --add-drop-table --add-locks --create-options --disable-keys\
         --extended-insert --single-transaction --quick --set-charset --events --routines\
-        --triggers --tab=/tmp/$database $database $table;
+        --triggers --tab=/opt/mysql/backup/$database $database $table;
         done
     done
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "START SLAVE;"
--- runner/mysql_cluster_setup.sh
+++ runner/mysql_cluster_setup.sh
@@ -14,12 +14,13 @@ do
     echo "Waiting for mysql_slave database connection..."
     sleep 4
 done
-
-MS_STATUS=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "SHOW MASTER STATUS"`
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "FLUSH TABLES WITH READ LOCK;"
+MS_STATUS=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "SHOW MASTER STATUS;"`
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
 start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='replica',MASTER_PASSWORD='replicapass',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
 
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "$start_slave_stmt"
+export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "UNLOCK TABLES;"
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "SHOW SLAVE STATUS \G"
