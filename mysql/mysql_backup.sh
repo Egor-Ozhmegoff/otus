330b8c45a09a244b1ca31070dc23b6460c959a05
Egor <eozhmegov@ozon.ru>
2023-07-20 19:07:29 +0300

    runner added

--- mysql/mysql_backup.sh
+++ mysql/mysql_backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-rm -r /opt/backup/*_db 2> /dev/null
-export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.131 -e "STOP SLAVE;"
-MYSQL=`export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.131 --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
+rm -r ./backup/*_db 2> /dev/null
+sudo mysql -u grafana -h 10.110.1.131 -e "STOP SLAVE;"
+MYSQL=`sudo mysql --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
     do
-    TABLES=`export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.131 --skip-column-names -e "use $database; show tables;"`
-    mkdir -m 777 /opt/backup/$database;
+    TABLES=`sudo mysql --skip-column-names -e "use $database; show tables;"`
+    mkdir -m 777 ./backup/$database;
     for table in $TABLES;
         do
-        export MYSQL_PWD=grafana; mysqldump -u grafana -h 10.110.1.131 --add-drop-table --add-locks --create-options --disable-keys\
+        export MYSQL_PWD=grafana; mysqldump --add-drop-table --add-locks --create-options --disable-keys\
         --extended-insert --single-transaction --quick --set-charset --events --routines\
-        --triggers --tab=/opt/backup/$database $database $table;
+        --triggers --tab=./backup/$database $database $table;
         done
     done
-export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.131 -e "START SLAVE;"
+sudo mysql -e "START SLAVE;"
