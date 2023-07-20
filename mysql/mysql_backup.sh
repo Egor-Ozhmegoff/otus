b4ae971167bc783844c16ec112f31fc9e55f0aaa
Egor <eozhmegov@ozon.ru>
2023-07-20 18:50:53 +0300

    runner added

--- mysql/mysql_backup.sh
+++ mysql/mysql_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -r ./backup/*_db 2> /dev/null
-export MYSQL_PWD=grfana; mysql -u grafana -h 10.110.1.131-e "STOP SLAVE;"
+export MYSQL_PWD=grfana; mysql -u grafana -h 10.110.1.131 -e "STOP SLAVE;"
 MYSQL=`export MYSQL_PWD=rootpass; mysql -u grafana -h 10.110.1.131 --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
