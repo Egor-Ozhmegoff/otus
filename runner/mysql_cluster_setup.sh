796e8a37e5502f101494aa687c12d848f6082acf
Egor <eozhmegov@ozon.ru>
2023-07-20 10:02:44 +0300

    runner added

--- runner/mysql_cluster_setup.sh
+++ runner/mysql_cluster_setup.sh
@@ -19,7 +19,7 @@ MS_STATUS=`export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "SHOW MAS
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-slave_config="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='replica',MASTER_PASSWORD='replicapass',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS,GET_MASTER_PUBLIC_KEY=1;"
+slave_config="CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql_master',SOURCE_USER='replica',SOURCE_PASSWORD='replicapass',SOURCE_LOG_FILE='$CURRENT_LOG',SOURCE_LOG_POS=$CURRENT_POS,GET_SOURCE_PUBLIC_KEY=1;"
 
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_slave -e "$slave_config"
 export MYSQL_PWD=rootpass; mysql -u root -h mysql_master -e "START SLAVE;"
