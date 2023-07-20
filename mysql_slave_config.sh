b4f64444bf42dd09db97f43386e4c535937192a8
Egor <eozhmegov@ozon.ru>
2023-07-20 18:40:41 +0300

    runner added

--- mysql_slave_config.sh
+++ mysql_slave_config.sh
@@ -7,7 +7,7 @@ MS_STATUS=`export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.130 -e "SHOW M
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-slave_config="CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql_master',SOURCE_USER='replica',SOURCE_PASSWORD='replicapass',SOURCE_LOG_FILE='$CURRENT_LOG',SOURCE_LOG_POS=$CURRENT_POS,GET_SOURCE_PUBLIC_KEY=1;"
+slave_config="CHANGE REPLICATION SOURCE TO SOURCE_HOST='10.110.1.130',SOURCE_USER='replica',SOURCE_PASSWORD='replicapass',SOURCE_LOG_FILE='$CURRENT_LOG',SOURCE_LOG_POS=$CURRENT_POS,GET_SOURCE_PUBLIC_KEY=1;"
 sudo mysql -e "STOP SLAVE;"
 sudo mysql -e "$slave_config"
 sudo mysql -e "START SLAVE;"
