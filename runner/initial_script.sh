afe9def3a2ef44fc12f63efae073ae5bfab73020
Egor <eozhmegov@ozon.ru>
2023-07-20 15:23:08 +0300

    runner added

--- runner/initial_script.sh
+++ runner/initial_script.sh
@@ -1,3 +1,6 @@
 bash /opt/mysql/mysql_cluster_setup.sh >> /var/log/mysql_config.log 2>&1
 cron
 sleep infinity
+eval `ssh-agent -s`
+ssh-add /root/.ssh/otus
+git clone git clone git@github.com:Egor-Ozhmegoff/backup.git /opt/mysql
