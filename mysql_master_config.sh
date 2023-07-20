7b53f162f1dda543cf57a9fdd121a5a12d438d34
Egor <eozhmegov@ozon.ru>
2023-07-20 17:53:53 +0300

    runner added

--- mysql_master_config.sh
+++ mysql_master_config.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-copy ./mysql/master/mysql.conf.cnf /etc/mysql/mysql.d/mysql.conf.cnf
+echo 'server-id = 1' >> /etc/mysql/mysql.conf.d/mysql.cnf
+echo 'bind-address = 0.0.0.0' >> /etc/mysql/mysql.conf.d/mysql.cnf
+echo 'secure-file-priv=""' >> /etc/mysql/mysql.conf.d/mysql.cnf
 
 priv_stmt='CREATE USER "replica"@"%" IDENTIFIED WITH 'caching_sha2_password' BY "replicapass"; GRANT REPLICATION SLAVE ON *.* TO "replica"@"%"; FLUSH PRIVILEGES;'
-mysql -u root -e "$priv_stmt"
-mysql -u root -e "CREATE USER "grafana"@"%" IDENTIFIED  BY "grafana";GRANT ALL PRIVILEGES ON *.* TO "grafana"@"%";FLUSH PRIVILEGES;"
-mysql -u root -e "CREATE DATABASE grfana_db;"
+sudo mysql -u root -e "$priv_stmt"
+sudo mysql -u root -e "CREATE USER "grafana"@"%" IDENTIFIED  BY "grafana";GRANT ALL PRIVILEGES ON *.* TO "grafana"@"%";FLUSH PRIVILEGES;"
+sudo mysql -u root -e "CREATE DATABASE grfana_db;"
