0f3db9fd490abab9ee36a04ac0395688936be7e0
Egor <eozhmegov@ozon.ru>
2023-07-20 17:59:58 +0300

    runner added

--- mysql_master_config.sh
+++ mysql_master_config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo 'server-id = 1' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-echo 'bind-address = 0.0.0.0' >> /etc/mysql/mysql.conf.d/mysqld.cnf
-echo 'secure-file-priv=""' >> /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo copy ./mysql/master/mysql.conf.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 priv_stmt='CREATE USER "replica"@"%" IDENTIFIED WITH 'caching_sha2_password' BY "replicapass"; GRANT REPLICATION SLAVE ON *.* TO "replica"@"%"; FLUSH PRIVILEGES;'
 sudo mysql -u root -e "$priv_stmt"
