375ffa5765acbc9524a8029aa9b5b8d3db1267c9
Egor <eozhmegov@ozon.ru>
2023-07-20 18:10:08 +0300

    runner added

--- mysql_master_config.sh
+++ mysql_master_config.sh
@@ -3,6 +3,6 @@
 sudo cp ./mysql/master/mysql.conf.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
 priv_stmt='CREATE USER "replica"@"%" IDENTIFIED WITH 'caching_sha2_password' BY "replicapass"; GRANT REPLICATION SLAVE ON *.* TO "replica"@"%"; FLUSH PRIVILEGES;'
-sudo mysql -u root -e "$priv_stmt"
-sudo mysql -u root -e "CREATE USER "grafana"@"%" IDENTIFIED  BY "grafana";GRANT ALL PRIVILEGES ON *.* TO "grafana"@"%";FLUSH PRIVILEGES;"
-sudo mysql -u root -e "CREATE DATABASE grfana_db;"
+sudo mysql -e "$priv_stmt"
+sudo mysql -e "CREATE USER "grafana"@"%" IDENTIFIED  BY "grafana";GRANT ALL PRIVILEGES ON *.* TO "grafana"@"%";FLUSH PRIVILEGES;"
+sudo mysql -e "CREATE DATABASE grafana_db;"
---
+++ mysql_slave_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.130 -e "FLUSH TABLES WITH READ LOCK;"
+MS_STATUS=`export MYSQL_PWD=grafana; mysql -u grafana -h 10.110.1.130 -e "SHOW MASTER STATUS;"`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+
+slave_config="CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql_master',SOURCE_USER='replica',SOURCE_PASSWORD='replicapass',SOURCE_LOG_FILE='$CURRENT_LOG',SOURCE_LOG_POS=$CURRENT_POS,GET_SOURCE_PUBLIC_KEY=1;"
+
+sudo mysql -e "$slave_config"
+sudo mysql -e "START SLAVE;"
+export MYSQL_PWD=grafana; mysql -u root -h 10.110.1.130 -e "UNLOCK TABLES;"
+sudo mysql -e "SHOW SLAVE STATUS \G"
