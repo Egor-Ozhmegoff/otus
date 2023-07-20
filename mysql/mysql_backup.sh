929d46fa6a8530e362889906b514ceb5297be07b
Egor <eozhmegov@ozon.ru>
2023-07-20 19:12:38 +0300

    runner added

--- mysql/mysql_backup.sh
+++ mysql/mysql_backup.sh
@@ -5,7 +5,7 @@ MYSQL=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "SHOW D
 
 for database in $MYSQL;
     do
-    TABLES=`export MYSQL_PWD=grafana; mysql -u grafana--skip-column-names -e "use $database; show tables;"`
+    TABLES=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "use $database; show tables;"`
     mkdir -m 777 ./backup/$database;
     for table in $TABLES;
         do
