4105a77aa7736e44e76ce2303b4c97e9e5d66714
Egor <eozhmegov@ozon.ru>
2023-07-22 01:25:51 +0300

    worldpress added

--- mysql/mysql_backup.sh
+++ mysql/mysql_backup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-rm -r /tmp/*_db 2> /dev/null
+c
+rm -r /tmp/backup/*_db 2> /dev/null
 export MYSQL_PWD=grafana; mysql -u grafana -e "STOP SLAVE;"
 MYSQL=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
     do
     TABLES=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "use $database; show tables;"`
-    mkdir -m 777 /tmp/$database;
+    mkdir -m 777 /tmp/backup$database;
     for table in $TABLES;
         do
         export MYSQL_PWD=grafana; mysqldump -u grafana --add-drop-table --add-locks --create-options --disable-keys\
         --extended-insert --single-transaction --quick --set-charset --events --routines\
-        --triggers --tab=/tmp/$database $database $table;
+        --triggers --tab=/tmp/backup$database $database $table;
         done
     done
 export MYSQL_PWD=grafana; mysql -u grafana -e "START SLAVE;"
--- runner/initial_script.sh
+++
@@ -1,3 +0,0 @@
-bash /opt/mysql/mysql_cluster_setup.sh >> /var/log/mysql_config.log 2>&1
-cron
-sleep infinity
