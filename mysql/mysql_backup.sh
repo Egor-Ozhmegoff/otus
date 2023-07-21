65b89fe816a94c7ecbfb7a0bdacb17db478f3e8d
Egor <eozhmegov@ozon.ru>
2023-07-22 00:01:26 +0300

    runner added

--- mysql/mysql_backup.sh
+++ mysql/mysql_backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-rm -r ./backup/*_db 2> /dev/null
+rm -r /tmp/*_db 2> /dev/null
 export MYSQL_PWD=grafana; mysql -u grafana -e "STOP SLAVE;"
 MYSQL=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "SHOW DATABASES LIKE '%\_db';"`
 
 for database in $MYSQL;
     do
     TABLES=`export MYSQL_PWD=grafana; mysql -u grafana --skip-column-names -e "use $database; show tables;"`
-    mkdir -m 777 ./backup/$database;
+    mkdir -m 777 /tmp/$database;
     for table in $TABLES;
         do
         export MYSQL_PWD=grafana; mysqldump -u grafana --add-drop-table --add-locks --create-options --disable-keys\
         --extended-insert --single-transaction --quick --set-charset --events --routines\
-        --triggers --tab=./backup/$database $database $table;
+        --triggers --tab=/tmp/$database $database $table;
         done
     done
 export MYSQL_PWD=grafana; mysql -u grafana -e "START SLAVE;"
